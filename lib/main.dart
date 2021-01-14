420dc9d1714ef72c4ebf68ca8ff6875cd3d34732
bhupendrapt95 <bhupendrapt95@gmail.com>
2021-01-15 01:12:27 +0530

    added vol controller

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:video_player_task/video_screen.dart';
+import 'package:video_player_task/clips.dart';
+import 'package:video_player_task/play_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,12 +12,104 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Video Player',
+      title: 'Video List Play Demo',
       theme: ThemeData(
-        primarySwatch: Colors.teal,
+        primarySwatch: Colors.lime,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: VideoScreen(),
+      home: IntroPage(),
+    );
+  }
+}
+
+class IntroPage extends StatefulWidget {
+  IntroPage({Key key}) : super(key: key);
+
+  @override
+  _IntroPageState createState() => _IntroPageState();
+}
+
+class _IntroPageState extends State<IntroPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                height: 120,
+              ),
+              Text(
+                "Playing Video list Workout",
+                style: TextStyle(
+                  fontSize: 24.0,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    MaterialButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(32)),
+                          side: BorderSide(color: Colors.black45, width: 1)),
+                      color: Colors.white,
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          CupertinoPageRoute(
+                              builder: (context) =>
+                                  PlayPage(clips: VideoClip.localClips)),
+                        );
+                      },
+                      minWidth: 240,
+                      padding:
+                          EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                      child: Text(
+                        "Play local files",
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    MaterialButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(32)),
+                          side: BorderSide(color: Colors.black45, width: 1)),
+                      color: Colors.white,
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          CupertinoPageRoute(
+                              builder: (context) =>
+                                  PlayPage(clips: VideoClip.remoteClips)),
+                        );
+                      },
+                      minWidth: 240,
+                      padding:
+                          EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                      child: Text(
+                        "Play remote files",
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      )),
     );
   }
 }
